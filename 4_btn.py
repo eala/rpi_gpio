80169f6004972eae472fee15cdbb9246e6f8dd12
eala <eala0113@gmail.com>
2020-10-27 01:29:05 +0800

    [btn] for btn polling, add command interval
    1. to avoid noise trigger

--- 4_btn.py
+++ 4_btn.py
@@ -3,15 +3,22 @@ import time
 
 def main():
     BTN_PIN = 11
+    MIN_TIME_INTERVAL = 0.4 # sec
+
+    # de-bounce when pressed
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(BTN_PIN, GPIO.IN)
 
     prev_status = None
+    prev_time = time.time()
     try:
         while True:
             input = GPIO.input(BTN_PIN)
-            if input == GPIO.LOW and prev_status == GPIO.HIGH:
+            cur_time = time.time()
+            if input == GPIO.LOW and prev_status == GPIO.HIGH and \
+                    (cur_time - prev_time > MIN_TIME_INTERVAL):
                 print("button pressed " + time.ctime())
+                prev_time = cur_time
             prev_status = input
 
     except KeyboardInterrupt:
